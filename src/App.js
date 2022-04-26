02957a59bb792265ab9b0a56174bb34558dc557d
kjin <iyneed4573@gmail.com>
2022-04-26 13:32:20 +0900

    Publishing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/abot-us" element={<h1>Hello</h1>} />
       </Routes>
